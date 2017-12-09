6755a07006ba98e17fbacc4b68aa4d13ee565c95
Majid Asgari <majid.asgari@gmail.com>
2017-12-09 15:13:47 +0330

    remove cache folder from config

--- src/main/java/ir/ac/iust/dml/kg/resource/extractor/services/web/rest/EntityExtractorRestServices.java
+++ src/main/java/ir/ac/iust/dml/kg/resource/extractor/services/web/rest/EntityExtractorRestServices.java
@@ -1,16 +1,15 @@
 package ir.ac.iust.dml.kg.resource.extractor.services.web.rest;
 
 import io.swagger.annotations.Api;
+import ir.ac.iust.dml.kg.raw.utils.ConfigReader;
 import ir.ac.iust.dml.kg.resource.extractor.IResourceExtractor;
 import ir.ac.iust.dml.kg.resource.extractor.IResourceReader;
 import ir.ac.iust.dml.kg.resource.extractor.MatchedResource;
 import ir.ac.iust.dml.kg.resource.extractor.ResourceCache;
-import ir.ac.iust.dml.kg.resource.extractor.services.Application;
 import ir.ac.iust.dml.kg.resource.extractor.tree.TreeResourceExtractor;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
@@ -22,12 +21,7 @@ public class EntityExtractorRestServices {
 
     public EntityExtractorRestServices() throws Exception {
         extractor = new TreeResourceExtractor();
-//        try (IResourceReader reader = new ResourceReaderFromKGStoreV1Service("http://localhost:8091/")) {
-//            extractor.setup(reader, 10000);
-//        }
-        final String jarFilePath = Application.class.getProtectionDomain()
-            .getCodeSource().getLocation().toURI().getPath();
-        final Path cacheAddress = Paths.get(jarFilePath).getParent().resolve("cache");
+      final Path cacheAddress = ConfigReader.INSTANCE.getPath("searcher.cache.dir", "cache");
         try (IResourceReader reader = new ResourceCache(cacheAddress.toString(), true)) {
           extractor.setup(reader, 10000);
         }
